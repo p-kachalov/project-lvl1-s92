be9c183a4c5bc22f3b0088e4f66aa248c071a29e
pavel-k <pa.kachalov@gmail.com>
2017-04-07 17:04:23 +0300

    refactor flow

--- src/index.js
+++ src/index.js
@@ -1,44 +1,35 @@
 import readlineSync from 'readline-sync';
 import { car, cdr } from 'hexlet-pairs';
 
-const playRound = (puzzle) => {
-  const question = car(puzzle);
-  const answer = cdr(puzzle);
-  console.log(`Question: ${question}`);
-  const userAnswer = readlineSync.question('Your answer: ');
-  if (userAnswer === answer) {
-    console.log('Correct!');
-    return true;
-  }
-  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
-  return false;
-};
+const attemptsCount = 3;
 
-const playGame = (username, attemptsCount, getPuzzle) => {
+const playGame = (username, getPuzzle) => {
   const iter = (counter) => {
-    if (counter === 0) {
-      console.log(`Congratulations, ${username}!`);
-      return;
+    if (counter === 0) return true;
+    const puzzle = getPuzzle();
+    console.log(`Question: ${car(puzzle)}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === cdr(puzzle)) {
+      console.log('Correct!');
+      return iter(counter - 1);
     }
-    if (!playRound(getPuzzle())) {
-      console.log(`Let's try again, ${username}!`);
-      return;
-    }
-    iter(counter - 1);
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${cdr(puzzle)}'.`);
+    return false;
   };
 
-  iter(attemptsCount);
+  return iter(attemptsCount);
 };
 
 export default (conditions, getPuzzle) => {
   console.log('Welcome to the Brain Games!');
-  if (conditions) console.log(conditions);
+  console.log(conditions);
 
   const username = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello, ${username}!\n`);
 
-  if (conditions) {
-    const attemptsCount = 3;
-    playGame(username, attemptsCount, getPuzzle);
+  if (playGame(username, getPuzzle)) {
+    console.log(`Congratulations, ${username}!`);
+  } else {
+    console.log(`Let's try again, ${username}!`);
   }
 };
